c435d8e8cafcf420f8ddb065b3b65b2e26177682
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-11 12:52:07 +0800

    jf: gps: Remove unused izat config
    This config merely sets what is already the default.
    Furthermore, Stock GPE and TW don't use nor have this config.
    
    Change-Id: I5a3991e85bf6b2882798debb77f0a10eb5934479

--- jfvelte.mk
+++ jfvelte.mk
@@ -119,8 +119,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf \
-    $(LOCAL_PATH)/configs/izat.conf:/system/etc/izat.conf
+    $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
