67fa43ef9792bc1f1c2133c4333cc660b14513db
sombree <sombree@gmail.com>
2017-01-09 18:43:44 +0100

    camerawrapper: there is no need to map iso values

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -114,10 +114,6 @@ static char *camera_fixup_getparams(int id, const char *settings)
                 "auto,action,night,sunset,party");
     }
 
-    /* Photos: Correct exposed ISO values */
-    params.set(CameraParameters::KEY_SUPPORTED_ISO_MODES,
-            "auto,ISO_HJR,ISO100,ISO200,ISO400,ISO800,ISO1600");
-
     ALOGV("%s: Fixed parameters:", __FUNCTION__);
     params.dump();
 
@@ -141,21 +137,6 @@ static char *camera_fixup_setparams(int id, const char *settings)
     /* Rear videos: Correct camera mode to 0 */
     if (isVideo && id == REAR_CAMERA_ID) {
         params.set(CameraParameters::KEY_CAMERA_MODE, "0");
-    }
-
-    /* Photos: Map the corrected ISO values to the ones in the HAL */
-    const char *isoMode = params.get(CameraParameters::KEY_ISO_MODE);
-    if (isoMode) {
-        if (!strcmp(isoMode, "ISO100"))
-            params.set(CameraParameters::KEY_ISO_MODE, "100");
-        else if (!strcmp(isoMode, "ISO200"))
-            params.set(CameraParameters::KEY_ISO_MODE, "200");
-        else if (!strcmp(isoMode, "ISO400"))
-            params.set(CameraParameters::KEY_ISO_MODE, "400");
-        else if (!strcmp(isoMode, "ISO800"))
-            params.set(CameraParameters::KEY_ISO_MODE, "800");
-        else if (!strcmp(isoMode, "ISO1600"))
-            params.set(CameraParameters::KEY_ISO_MODE, "1600");
     }
 
     ALOGV("%s: Fixed parameters:", __FUNCTION__);
