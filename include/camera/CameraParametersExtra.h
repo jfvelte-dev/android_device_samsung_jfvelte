9e677581b98f27f90423f71ffc0a498bcd23e342
sombree <sombree@gmail.com>
2017-01-13 20:25:05 +0100

    camera: remove unused parameters from header

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -16,10 +16,6 @@
 
 #define CAMERA_PARAMETERS_EXTRA_C \
 const char CameraParameters::KEY_CAMERA_MODE[] = "camera-mode"; \
-const char CameraParameters::KEY_ISO_MODE[] = "iso"; \
-const char CameraParameters::KEY_SUPPORTED_ISO_MODES[] = "iso-values";
 
 #define CAMERA_PARAMETERS_EXTRA_H \
     static const char KEY_CAMERA_MODE[]; \
-    static const char KEY_ISO_MODE[]; \
-    static const char KEY_SUPPORTED_ISO_MODES[];
